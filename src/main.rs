e0c91d5f1328041dc6bb2a2071ebc279ffe1aa7a
Francois Best <francois@francoisbest.com>
2019-01-24 21:04:31 +0100

    feat: Add URL building and open in browser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
 use structopt::StructOpt;
+use webbrowser;
+
+fn make_strava_auth_url(client_id: u32) -> String {
+  let scopes = [
+    // "read", // Shadowed by read_all
+    "read_all",
+    "profile:read_all",
+    "profile:write",
+    // "activity:read", // Shadowed by activity:read_all
+    "activity:read_all",
+    "activity:write",
+  ]
+  .join(",");
+
+  let params = [
+    format!("client_id={}", client_id),
+    String::from("redirect_uri=http://localhost:8000"),
+    String::from("response_type=code"),
+    String::from("approval_prompt=auto"),
+    format!("scope={}", scopes),
+  ]
+  .join("&");
+  format!("https://www.strava.com/oauth/authorize?{}", params)
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "strava-auth")]
@@ -15,5 +39,11 @@ struct CliArgs {
 
 fn main() {
   let cli_args = CliArgs::from_args();
-  println!("{:#?}", cli_args);
+
+  let auth_url = make_strava_auth_url(cli_args.client_id);
+  if webbrowser::open(&auth_url).is_err() {
+    // Try manually
+    println!("Visit the following URL to authorize your app with Strava:");
+    println!("{}\n", auth_url);
+  }
 }
