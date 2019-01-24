a4e20320a7de406fd9785bc5495efd6a85b7d932
Francois Best <francois@francoisbest.com>
2019-01-24 21:00:44 +0100

    feat: Add CLI arguments parsing

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "strava-auth")]
+/// Authorize and authenticate a Strava API app.
+///
+/// Requires a GUI web browser to be available.
+struct CliArgs {
+  #[structopt(short = "i", long = "id")]
+  client_id: u32,
+
+  #[structopt(short = "s", long = "secret")]
+  client_secret: String,
+}
+
+fn main() {
+  let cli_args = CliArgs::from_args();
+  println!("{:#?}", cli_args);
+}
